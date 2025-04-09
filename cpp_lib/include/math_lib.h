0cdf81577d17ea60bffb122aa3b3f0863198b1eb
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-08 21:08:27 -0400

    Adjusted h file for clarity and c# app

--- cpp_lib/include/math_lib.h
+++ cpp_lib/include/math_lib.h
@@ -1,4 +1,10 @@
-extern "C" double add(double a, double b);
+#ifndef MATH_LIB_H
+#define MATH_LIB_H
+
+extern "C" {
+  double add(double a, double b);
 double subtract_doubles(double a, double b);
 double multiply_doubles(double a, double b);
 double divide_doubles(double a, double b);
+}
+#endif
