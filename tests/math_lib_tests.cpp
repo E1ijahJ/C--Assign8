c293862a7e4426141e819df2b568df8baabb1500
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-08 20:49:50 -0400

    Added the test for the subtraction function

--- tests/math_lib_tests.cpp
+++ tests/math_lib_tests.cpp
@@ -4,3 +4,9 @@
 TEST(AdditionTest, PositiveNumbers) {
     EXPECT_DOUBLE_EQ(add(2.5, 3.5), 6.0);
 }
+
+TEST(MathLibTests, SubtractDoubles) {
+    EXPECT_DOUBLE_EQ(subtract_doubles(5.5, 2.5), 3.0);
+    EXPECT_DOUBLE_EQ(subtract_doubles(-1.0, -1.0), 0.0);
+    EXPECT_DOUBLE_EQ(subtract_doubles(0.0, 3.0), -3.0);
+}
